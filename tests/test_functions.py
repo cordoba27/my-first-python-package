47234a2397df021796293f1e056c1400e6e78241
cordoba27 <seb.schmidt2710@gmail.com>
2024-12-05 15:36:32 +0100

    Fixed path

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,8 @@
 import bastipy
 import unittest
+import os
 
 class TestFunction(unittest.TestCase):
 
   def test_functions(self):
-    self.assertIsInstance(bastipy.geo_counter(input_path="../data/NUTS_0.geojson"), int)
+    self.assertIsInstance(bastipy.geo_counter(input_path=r"data\NUTS_0.geojson"), int)
